08059d5886493632f2792f4c06fe5af046d37b0c
Raul Soto <raul@MacBook-Pro-de-Raul.local>
2019-08-07 10:48:02 -0500

    agregando nuevo archivo prueba

---
+++ src/main/java/com/mycompany/dto/Persona4.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.dto;
+
+/**
+ *
+ * @author Oracle D77738
+ */
+public class Persona4 {
+    private String nombre;
+    private String apellido;
+    private String ciudad;
+    private String estado;
+    private String municipio;
+
+    public Persona(String nombre, String apellido, String ciudad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.ciudad = ciudad;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+    
+    
+}
